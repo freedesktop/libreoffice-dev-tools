b26608fc9049ebfe583f2d92553fc2d60add0625
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2011-05-20 14:02:50 +0200

    abort on error, use clone instead of archive

--- ubuntu-jenkins/setup-ubuntu-jenkins.sh
+++ ubuntu-jenkins/setup-ubuntu-jenkins.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+set -e
 mkdir ${HOME}/.jenkins
 cd ${HOME}/.jenkins
 wget http://mirrors.jenkins-ci.org/war/latest/jenkins.war
 wget --no-check-certificate https://github.com/downloads/KentBeck/junit/junit-4.9b2.jar
-git archive --remote=git://anongit.freedesktop.org/libreoffice/contrib/dev-tools ubuntu-jenkins/jobs |tar x
+git clone git://anongit.freedesktop.org/libreoffice/contrib/dev-tools dev-tools
+mv dev-tools/ubuntu-jenkins/jobs jobs
+rm -rf dev-tools
 echo "done."
 echo "You can start your LibreOffice Ubuntu Jenkins server with: java -jar ~/.jenkins/jenkins.war"
 echo "It will then be running at http://localhost:8080"
