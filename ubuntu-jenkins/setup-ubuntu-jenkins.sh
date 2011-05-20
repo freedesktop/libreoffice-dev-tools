7de7dc46754e97064bf10666057a6b41bcaa1166
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2011-05-20 13:54:48 +0200

    not using cert checks because of certificate errors

--- ubuntu-jenkins/setup-ubuntu-jenkins.sh
+++ ubuntu-jenkins/setup-ubuntu-jenkins.sh
@@ -2,7 +2,7 @@
 mkdir ${HOME}/.jenkins
 cd ${HOME}/.jenkins
 wget http://mirrors.jenkins-ci.org/war/latest/jenkins.war
-wget http://github.com/downloads/KentBeck/junit/junit-4.9b2.jar
+wget --no-check-certificate https://github.com/downloads/KentBeck/junit/junit-4.9b2.jar
 git archive --remote=git://anongit.freedesktop.org/libreoffice/contrib/dev-tools ubuntu-jenkins/jobs |tar x
 echo "done."
 echo "You can start your LibreOffice Ubuntu Jenkins server with: java -jar ~/.jenkins/jenkins.war"
