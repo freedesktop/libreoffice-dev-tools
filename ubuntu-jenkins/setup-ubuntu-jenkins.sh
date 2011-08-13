2d5c9296be0b2e00720f6caea0eda26732678bf8
Henrik Jensen <hrikjsenvagt-lo-KK7TH6PgNEI@public.gmane.org>
2011-08-13 06:04:58 +0100

    Series of fixes and migration to One Git for contrib/dev-tools/ubuntu-jenkins
    A few more suggested patches to 'setup-ubuntu-jenkins.sh'
    
    ( committed to local clone after  0007-Install-Jenkins-in-current-dir-instead-of-.jenkins.patch
    so I guess they are dependent )
    
    Patch descriptions:
    ---------------------------------------------------------------------------------------------------
    0001-Add-install-path-to-cmdln.-args.patch:
    Add install path to cmdln. args.
    
    Per default installs in ~/.jenkins or the path added to the command line
    of setup-ubuntu-jenkins.sh
    
    ---------------------------------------------------------------------------------------------------
    0002-Add-simple-instructions-for-h-help.patch:
    Add simple instructions for -h/--help
    
    ---------------------------------------------------------------------------------------------------
    
    --Henrik Jensen
    (HenrikJ on #libreoffice-dev)
    
    From 5830b0fdb689e01c622bb10c29aba21985e9da2d Mon Sep 17 00:00:00 2001
    From: Henrik Jensen <hrikjsenvagt-lo@yahoo.dk>
    Date: Fri, 12 Aug 2011 12:41:44 +0200
    Subject: [PATCH 1/2] Add install path to cmdln. args.
    
    Per default installs in ~/.jenkins or the path added to the command line
    of setup-ubuntu-jenkins.sh

--- ubuntu-jenkins/setup-ubuntu-jenkins.sh
+++ ubuntu-jenkins/setup-ubuntu-jenkins.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 set -e
-mkdir jenkins
-cd jenkins
+case "$1" in
+	-h | --help)
+	cat << EOF
+Usage: $(basename "$0") [OPTION]... [PATH]
+Installs Jenkins in PATH if given or defaults to '~/.jenkins'
+
+Options:
+  -h, --help     display this help and exit
+EOF
+	exit 0;;
+esac
+instdir=${1:-~/.jenkins}
+mkdir -p "$instdir"
+cd "$instdir"
 wget http://mirrors.jenkins-ci.org/war/latest/jenkins.war
 wget --no-check-certificate https://github.com/downloads/KentBeck/junit/junit-4.9b2.jar
 git clone git://anongit.freedesktop.org/libreoffice/contrib/dev-tools dev-tools
 mv dev-tools/ubuntu-jenkins/jobs jobs
 rm -rf dev-tools
-cd ..
 echo "#!bin/sh" > start-lo-jenkins.sh
-echo "java -DJENKINS_HOME=$(pwd)/jenkins -jar $(pwd)/jenkins/jenkins.war">> start-lo-jenkins.sh
+echo "java -DJENKINS_HOME=$(pwd) -jar $(pwd)/jenkins.war">> start-lo-jenkins.sh
 chmod u+x start-lo-jenkins.sh
 echo "done."
 echo "You can start your LibreOffice Ubuntu Jenkins server with: $(pwd)/start-lo-jenkins.sh"
