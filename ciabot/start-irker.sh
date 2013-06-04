2d9fef095395a1a08ad5f9e5888716938b2adb71
Miklos Vajna <vmiklos@suse.cz>
2013-06-04 14:12:26 +0200

    ciabot: upgrade python-irc
    Change-Id: I636c85c324dd5abc050dc634b8cdcb43f32a2d8c

--- ciabot/start-irker.sh
+++ ciabot/start-irker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ~/bin/irker
-export PYTHONPATH=irc-8.0.1
+export PYTHONPATH=irc-8.3
 if test -z "`ps ax | grep irkerd | grep -v grep`"; then
 	./irkerd -n loirkerbot &>irkerd.log &
 fi
