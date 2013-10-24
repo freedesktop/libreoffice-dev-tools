1519185e1c84885ad9c68852282c76e14c64bb01
Michael Meeks <michael.meeks@novell.com>
2013-10-24 18:56:26 +0200

    make our own make detectable by adding lines to --version

--- make-4.0-gbuild/main.c
+++ make-4.0-gbuild/main.c
@@ -3282,6 +3282,7 @@ print_version (void)
 %sThis is free software: you are free to change and redistribute it.\n\
 %sThere is NO WARRANTY, to the extent permitted by law.\n"),
             precede, precede, precede);
+  printf ("LibreOffice patched version\n");
 
   printed_version = 1;
 
