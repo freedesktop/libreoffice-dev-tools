72a4409de8c33f4bdccdc6770c0ac0e8a6c0e0e8
Markus Mohrhard <markus.mohrhard@googlemail.com>
2014-11-06 18:42:05 -0500

    update control scripts from VM

--- test-bugzilla-files/commands.sh
+++ test-bugzilla-files/commands.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
 cd /srv/crashtestdata/
+source config.cfg
 rm tmpdir/* -r
 rm current/* -r
 rm control/* -r
+rm console_*
 cd ~/build/
 make clean
 cd ~/source/dev-tools/
@@ -15,11 +18,19 @@ cd ~/build/
 make
 
 if [ "$?" != "0" ]; then
-    echo "Please inspect the build" | mail -s "Crash test VM build failure" markus.mohrhard@googlemail.com
+	/srv/crashtestdata/sendEmail -f crashtest.libreoffice@gmail.com -t markus.mohrhard@googlemail.com -o tls=yes -s smtp.gmail.com:587 -u "Crash test build failure" -m "The build failed. Please check!" -xu crashtest.libreoffice -xp $PASSWORD
+	/srv/crashtestdata/sendEmail -f crashtest.libreoffice@gmail.com -t vmiklos@collabora.co.uk -o tls=yes -s smtp.gmail.com:587 -u "Crash test build failure" -m "The build failed. Please check!" -xu crashtest.libreoffice -xp $PASSWORD
     exit 1
 fi
 
-cd /srv/crashtestdata/files/
-python3 new-control.py
+cd ~/source/dev-tools/test-bugzilla-files/
+python3 new-control.py /srv/crashtestdata/files/
+
+if [ "$?" != "0" ]; then
+	/srv/crashtestdata/sendEmail -f crashtest.libreoffice@gmail.com -t markus.mohrhard@googlemail.com -o tls=yes -s smtp.gmail.com:587 -u "Crash test failure" -m "The test run failed. Please check!" -xu crashtest.libreoffice -xp $PASSWORD
+	/srv/crashtestdata/sendEmail -f crashtest.libreoffice@gmail.com -t vmiklos@collabora.co.uk -o tls=yes -s smtp.gmail.com:587 -u "Crash test failure" -m "The test run failed. Please check!" -xu crashtest.libreoffice -xp $PASSWORD
+    exit 1
+fi
+
 cd /srv/crashtestdata/
 ./zip.sh
--- test-bugzilla-files/zip.sh
+++ test-bugzilla-files/zip.sh
@@ -1,14 +1,15 @@
 cd ~/source/libo-core/
 SHA=`git rev-parse HEAD`
 echo $SHA
+cd /srv/crashtestdata/
+source config.cfg
 cd /srv/crashtestdata/control/
-zip -r control.zip */*
-mv control.zip /srv/crashtestdata/logs/$SHA/.
+cat */crashlog.txt > /srv/crashtestdata/logs/$SHA/crashlog.txt
+cat */exportCrash.txt > /srv/crashtestdata/logs/$SHA/exportCrash.txt
 cd /srv/crashtestdata/current/srv/crashtestdata/files/
 zip -r validation.zip */*.log
 mv validation.zip /srv/crashtestdata/logs/$SHA/.
 cd /srv/crashtestdata/logs/$SHA
-unzip control.zip
 unzip validation.zip -d validation
 rm *.zip
 cp ~/source/dev-tools/test-bugzilla-files/analyze-logs.py .
@@ -22,3 +23,5 @@ ssh upload@gimli.documentfoundation.org 'bash -s' << 'ENDSSH'
 cd /srv/www/dev-builds.libreoffice.org/crashtest/
 unzip current.zip
 ENDSSH
+cd /srv/crashtestdata/
+./sendEmail -f "Crashtest VM <crashtest.libreoffice@gmail.com>" -t libreoffice@lists.freedesktop.org -o tls=yes -s smtp.gmail.com:587 -u "Crash test update" -m "New crashtest update available at http://dev-builds.libreoffice.org/crashtest/$SHA/" -a logs/*.csv -xu crashtest.libreoffice -xp $PASSWORD
