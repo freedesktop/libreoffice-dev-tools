d7c8e6f5c3993dd3bea2dce1e983c7259f8db7c6
Maarten Hoes <hoes.maarten@gmail.com>
2015-01-26 09:09:10 +0100

    set LANG and LC_* to fix 'UnicodeEncodeError' when running from cron.
    Change-Id: Ibff06172c10a73048871f8569b083e39526828fd
    Reviewed-on: https://gerrit.libreoffice.org/14181
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- cppcheck/cppcheck-report.sh
+++ cppcheck/cppcheck-report.sh
@@ -205,7 +205,22 @@ SENDEMAIL=~/source/buildbot/bin/sendEmail
 EMAIL_BODY=~/tmp/email_body
 # Dont forget to set SMTP_PWD in your ~/.cppcheckrc !
 SMTP_PWD=
-export PYTHONIOENCODING=utf-8
+export PYTHONIOENCODING=UTF-8
+
+export LANG=en_US.UTF-8
+export LC_CTYPE="en_US.UTF-8"
+export LC_NUMERIC="en_US.UTF-8"
+export LC_TIME="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
+export LC_MONETARY="en_US.UTF-8"
+export LC_MESSAGES="en_US.UTF-8"
+export LC_PAPER="en_US.UTF-8"
+export LC_NAME="en_US.UTF-8"
+export LC_ADDRESS="en_US.UTF-8"
+export LC_TELEPHONE="en_US.UTF-8"
+export LC_MEASUREMENT="en_US.UTF-8"
+export LC_IDENTIFICATION="en_US.UTF-8"
+export LC_ALL=en_US.UTF-8
 
 if [ -f ~/.cppcheckrc ]; then
     # override all default vars with entries in ~/.cppcheckrc
