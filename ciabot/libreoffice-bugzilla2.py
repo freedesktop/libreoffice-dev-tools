e11164c42cebd0c14356a449e4cfaa2a9a3529a7
Markus Mohrhard <markus.mohrhard@googlemail.com>
2015-04-02 23:59:15 +0200

    next version will be 5.0

--- ciabot/libreoffice-bugzilla2.py
+++ ciabot/libreoffice-bugzilla2.py
@@ -33,7 +33,7 @@ import bugzilla
 from bugzilla import Bugzilla
 from bugzilla.base import _BugzillaToken
 
-master_target = "4.5.0"
+master_target = "5.0.0"
 bug_regex = "(?:fdo|tdf)#(\d+)"
 
 class FreedesktopBZ:
