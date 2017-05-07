5ce255299da00e77e726e78ac8ece7db635bc3a0
jan Iversen <jani@libreoffice.org>
2017-05-07 16:08:32 +0200

    esc-report, first version of automate script

---
+++ esc-reporting/esc-automate.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+
+
+
+
+### DESCRIPTION
+#
+# This program automates update of bugzilla, gerrit and sends mail
+#
+# Installed on vm174:/usr/local/bin runs every night (generating and mailing reports)
+#
+#
+
+
+
+import sys
+import os
+import datetime
+import json
+
+
+def util_load_data_file(fileName):
+    try:
+      fp = open(fileName, encoding='utf-8')
+      rawData = json.load(fp)
+      fp.close()
+    except Exception as e:
+      print('Error load file ' + fileName + ' due to ' + str(e))
+      exit(-1)
+    return rawData
+
+
+
+def util_dump_file(fileName, rawList):
+    try:
+      fp = open(fileName, 'w', encoding='utf-8')
+      json.dump(rawList, fp, ensure_ascii=False, indent=4, sort_keys=True)
+      fp.close()
+    except Exception as e:
+      print('Error dump file ' + fileName + ' due to ' + str(e))
+      os.remove(fileName)
+      exit(-1)
+
+
+
+
+
+def runCfg(platform):
+    global cfg
+    if 'esc_homedir' in os.environ:
+      homeDir = os.environ['esc_homedir']
+    else:
+      homeDir = '/home/esc-mentoring/esc'
+
+    cfg = util_load_data_file(homeDir + '/config.json')
+    cfg['homedir'] = homeDir + '/'
+    cfg['platform'] = platform
+    print("Reading and writing data to " + cfg['homedir'])
+
+    cfg['award-mailed'] = util_load_data_file(cfg['homedir'] + 'award.json')['award-mailed']
+    cfg['nowDate'] = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    cfg['cutDate'] = cfg['nowDate'] - datetime.timedelta(days=365)
+    cfg['1weekDate'] = cfg['nowDate'] - datetime.timedelta(days=7)
+    cfg['1monthDate'] = cfg['nowDate'] - datetime.timedelta(days=30)
+    cfg['3monthDate'] = cfg['nowDate'] - datetime.timedelta(days=90)
+    cfg['1yearDate'] = cfg['nowDate'] - datetime.timedelta(days=365)
+    return cfg
+
+
+
+def runAutomate():
+    global cfg, autoList
+
+    autoList = util_load_data_file(cfg['homedir'] + 'automate.json')
+
+    xMail = []
+    try:
+      x = automate_gerrit()
+      if not x is None:
+        xMail.append(x)
+    except:
+      pass
+    try:
+      x = automate_bugzilla()
+      if not x is None:
+        xMail.append(x)
+    except:
+      pass
+    try:
+      x = automate_pdf()
+      if not x is None:
+        xMail.append(x)
+    except:
+      pass
+
+    fp = open('/tmp/runAutoMail', 'w', encoding='utf-8')
+    print("#!/bin/bash", file=fp)
+    print("")
+    for i in xMail:
+      if 'attach' in i:
+        attach = '-a ' + i['attach'] + ' '
+      else:
+        attach = ''
+      print("mail -s '" + i['title'] + "' " + attach + i['mail'] + " <  " + i['file'], file=fp)
+    fp.close()
+
+
+
+if __name__ == '__main__':
+    runCfg(sys.platform)
+    runAutomate()
