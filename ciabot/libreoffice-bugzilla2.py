47c6f434028c9b6a86f0f0cdb2a39aa05fad323c
Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
2017-11-27 17:48:39 +0100

    master is now on its way to 6.1.0...

--- ciabot/libreoffice-bugzilla2.py
+++ ciabot/libreoffice-bugzilla2.py
@@ -33,7 +33,7 @@ import bugzilla
 from bugzilla import Bugzilla
 from bugzilla.base import _BugzillaToken
 
-master_target = "6.0.0"
+master_target = "6.1.0"
 bug_regex = "(?:tdf|fdo)#(\d+)"
 dry_run = False
 
