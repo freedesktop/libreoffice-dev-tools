ce22e897f087288dba9aba1c5c83bfb97ffde83e
Xisco Fauli <xiscofauli@libreoffice.org>
2018-04-10 17:54:46 +0200

    ESC: Sending this email is not necessary

--- esc-reporting/esc-analyze.py
+++ esc-reporting/esc-analyze.py
@@ -244,7 +244,6 @@ def util_check_mail(name, xmail):
         else:
           # Return a fake email in order to not break the script
           mail = 'fake-email@fake-email-script-esc.com'
-          common.sendMail(cfg, 'xiscofauli@libreoffice.org', 'Error parsing email', 'ERROR parsing' + str(xmail))
 
     if mail in statList['aliases']:
       mail = statList['aliases'][mail]
