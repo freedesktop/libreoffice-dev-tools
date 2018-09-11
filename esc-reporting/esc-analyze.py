668d50e4fc33b218bfb6cdfdf24f9bd605317c85
Xisco Fauli <xiscofauli@libreoffice.org>
2018-09-11 17:59:58 +0200

    ESC: filter out Pootle bot and DUMMY

--- esc-reporting/esc-analyze.py
+++ esc-reporting/esc-analyze.py
@@ -786,7 +786,7 @@ def analyze_reports():
                 break
     tmpRlist = sorted(statList['people'], key=lambda k: (statList['people'][k]['gerrit']['1month']['reviewer']),reverse=True)
     for i in tmpRlist:
-        if i != 'ci@libreoffice.org':
+        if i != 'ci@libreoffice.org' and i != 'fake-email@fake-email-script-esc.com' and i != '*dummy*':
             x = {'mail': i, 'name': statList['people'][i]['name'],
                  'month': statList['people'][i]['gerrit']['1month']['reviewer'],
                  'year': statList['people'][i]['gerrit']['1year']['reviewer']}
