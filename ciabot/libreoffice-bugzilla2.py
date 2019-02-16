b122dcd8db04783314cb150fd2706ed6666b31a1
22shubh22 <22shubh22@gmail.com>
2019-02-16 19:53:07 +0530

    Documentation: - Working of script
    Change-Id: I2486c59fb956b7bc706fc859b1b6be7baa0bffdb
    Reviewed-on: https://gerrit.libreoffice.org/67905
    Reviewed-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>
    Tested-by: Thorsten Behrens <Thorsten.Behrens@CIB.de>

--- ciabot/libreoffice-bugzilla2.py
+++ ciabot/libreoffice-bugzilla2.py
@@ -13,6 +13,10 @@
 # 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# When a commit referencing a report is merged, this script
+# - adds a comment to the report
+# - updates the whiteboard field with target information
 
 from __future__ import print_function
 
