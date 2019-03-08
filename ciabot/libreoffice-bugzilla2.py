d979d3d34f5b3007f34f3fc664377af4921a1bb2
Shubham Goyal <22shubh22@gmail.com>
2019-03-08 13:19:51 +0530

    Code optimization in dev-tools/ciabot
    Previously the get_commit() func was called twice, now with one call,
    the program works as it is.
    
    Change-Id: I3f4ec4d2d94d9c86b1dc8879539a7dbfb3895327
    Reviewed-on: https://gerrit.libreoffice.org/67897
    Reviewed-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
    Tested-by: Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>

--- ciabot/libreoffice-bugzilla2.py
+++ ciabot/libreoffice-bugzilla2.py
@@ -167,15 +167,14 @@ def get_commit(repo, commit_id):
     commit = repo.commit(commit_id)
     return commit
 
-def find_bugid(repo, commit_id):
-    commit = get_commit(repo, commit_id)
+def find_bugid(commit):
     summary_line = commit.summary
     regex = re.compile(bug_regex)
     m = regex.findall(summary_line)
     if m is None or len(m) == 0:
         print("no bugid found")
         sys.exit()
-    
+
     return m
 
 def read_repo(repo_name):
@@ -220,9 +219,9 @@ def main(argv):
 
     target_version = find_target_version(repo, branch)
 
-    bug_ids = find_bugid(repo, commit_id)
-    
     commit = get_commit(repo, commit_id)
+
+    bug_ids = find_bugid(commit)
 
     if target_version is None:
         print("missing target version")
