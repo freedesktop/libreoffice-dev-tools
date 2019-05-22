cd22ccd55d3ecc2f4e4a51f9606beba73525aead
Caolán McNamara <caolanm@redhat.com>
2019-05-22 12:52:43 +0100

    keep cores

--- test-bugzilla-files/execute.sh
+++ test-bugzilla-files/execute.sh
@@ -1,7 +1,7 @@
+ulimit -c unlimited
 dir_name=$(basename $1)
 dir=${PWD}
 source ${dir}/config
 mkdir -p ${CRASHTESTDATA}/control/$dir_name
 cd ${CRASHTESTDATA}/control/$dir_name
 TMPDIR=${CRASHTESTDATA}/tmpdir ${INSTDIR}/program/python ${dir}/test-bugzilla-files.py --soffice=path:${INSTDIR}/program/soffice --userdir=file://${USERDIR}/libreoffice_$dir_name/4 $1 2>&1 | tee ${CRASHTESTDATA}/console_$dir_name.log
-rm core*
