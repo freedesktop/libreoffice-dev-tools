285863aebf635b4fa30a89daf6b36f50766757f7
Caolán McNamara <caolanm@redhat.com>
2019-05-22 12:56:59 +0100

    update command.sh to what we're using now

--- test-bugzilla-files/commands.sh
+++ test-bugzilla-files/commands.sh
@@ -15,20 +15,21 @@ SHA=`git rev-parse HEAD`
 echo $SHA
 mkdir /srv/crashtestdata/logs/$SHA
 cd ~/build/
-make
+make build-nocheck
 
 if [ "$?" != "0" ]; then
 	/srv/crashtestdata/sendEmail -f crashtest.libreoffice@gmail.com -t markus.mohrhard@googlemail.com -o tls=yes -s smtp.gmail.com:587 -u "Crash test build failure" -m "The build failed. Please check!" -xu crashtest.libreoffice -xp $PASSWORD
-	/srv/crashtestdata/sendEmail -f crashtest.libreoffice@gmail.com -t vmiklos@collabora.co.uk -o tls=yes -s smtp.gmail.com:587 -u "Crash test build failure" -m "The build failed. Please check!" -xu crashtest.libreoffice -xp $PASSWORD
+	/srv/crashtestdata/sendEmail -f crashtest.libreoffice@gmail.com -t caolanm@redhat.com -o tls=yes -s smtp.gmail.com:587 -u "Crash test build failure" -m "The build failed. Please check!" -xu crashtest.libreoffice -xp $PASSWORD
     exit 1
 fi
 
+ulimit -c unlimited
 cd ~/source/dev-tools/test-bugzilla-files/
 python3 new-control.py /srv/crashtestdata/files/
 
 if [ "$?" != "0" ]; then
 	/srv/crashtestdata/sendEmail -f crashtest.libreoffice@gmail.com -t markus.mohrhard@googlemail.com -o tls=yes -s smtp.gmail.com:587 -u "Crash test failure" -m "The test run failed. Please check!" -xu crashtest.libreoffice -xp $PASSWORD
-	/srv/crashtestdata/sendEmail -f crashtest.libreoffice@gmail.com -t vmiklos@collabora.co.uk -o tls=yes -s smtp.gmail.com:587 -u "Crash test failure" -m "The test run failed. Please check!" -xu crashtest.libreoffice -xp $PASSWORD
+	/srv/crashtestdata/sendEmail -f crashtest.libreoffice@gmail.com -t caolanm@redhat.com -o tls=yes -s smtp.gmail.com:587 -u "Crash test failure" -m "The test run failed. Please check!" -xu crashtest.libreoffice -xp $PASSWORD
     exit 1
 fi
 
