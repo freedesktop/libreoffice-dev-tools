freedesktop/libreoffice-dev-tools

commit f2204d126c2334ccbe061cd27d519316426448c1

ciabot: Port libreoffice-bugzilla2.py and irker-cia-proxy.py to python3.

ciabot/libreoffice-bugzilla2.py
@@ -18,23 +18,14 @@
 # - adds a comment to the report
 # - updates the whiteboard field with target information
 
-from __future__ import print_function
-
 import datetime
 import os
 import re
 import sys, getopt
 import git
-import ConfigParser
-
-if hasattr(sys.version_info, "major") and sys.version_info.major >= 3:
-# pylint: disable=F0401,E0611
-    from urllib.parse import urlparse
-else:
-    from urlparse import urlparse
+import configparser
 
 import bugzilla
-from bugzilla import Bugzilla
 
 master_target = "7.1.0"
 bug_regex = "\\b(?:bug|fdo|tdf|lo)[#:]?(\\d+)\\b"
@@ -46,7 +37,7 @@ class FreedesktopBZ:
     bz = None
 
     def connect(self):
-        config = ConfigParser.ConfigParser()
+        config = configparser.ConfigParser()
         config.read(os.path.dirname(os.path.abspath(__file__)) + '/config.cfg')
         url = config.get('bugzilla', 'url')
         user = config.get('bugzilla', 'user')
@@ -58,7 +49,7 @@ class FreedesktopBZ:
     def update_whiteboard(self, commit, bugnr, new_version, branch, repo_name):
         print(bugnr)
         if dry_run:
-            print("DRY RUN, we would set the whiteboard to: target:\n%s" % new_version)
+            print(("DRY RUN, we would set the whiteboard to: target:\n%s" % new_version))
         else:
             bug = self.bz.getbug(bugnr)
             print(bug)
@@ -99,7 +90,7 @@ https://wiki.documentfoundation.org/Testing_Daily_Builds
 Affected users are encouraged to test the fix and report feedback.""" %(new_version)
 
         if dry_run:
-            print("DRY RUN, we would add the following comment:\n%s" % comment_msg)
+            print(("DRY RUN, we would add the following comment:\n%s" % comment_msg))
         else:
             bug.addcomment(comment_msg)
 
@@ -177,7 +168,7 @@ def find_bugid(repo, commit_id):
     return m
 
 def read_repo(repo_name):
-    config = ConfigParser.ConfigParser()
+    config = configparser.ConfigParser()
     config.read(os.path.dirname(os.path.abspath(__file__)) + '/config.cfg')
     path = config.get(repo_name, 'location')
     repo = git.repo.base.Repo(path)

ciabot/irker-cia-proxy.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 irker-cia-proxy - proxy CIA requests to an irker relay agent
 
@@ -15,8 +15,8 @@ from the use of this software.
 
 import json, socket, posixpath, re, sys
 from xml.dom import minidom
-from SimpleXMLRPCServer import SimpleXMLRPCServer
-from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.server import SimpleXMLRPCRequestHandler
 
 bind_ip = ''
 bind_port = 8000
@@ -117,9 +117,9 @@ class CIAMessage:
     def get_template(self):
         # If there is a template for this branch, use it, otherwise fall back to the project or the global one.
         branch_template = "template-%s" % self.data()['branch']
-        if projmap[self.project()].has_key(branch_template):
+        if branch_template in projmap[self.project()]:
             return projmap[self.project()][branch_template]
-        if projmap[self.project()].has_key('template'):
+        if 'template' in projmap[self.project()]:
             return projmap[self.project()]['template']
         return template
     def get_target(self):
@@ -131,7 +131,7 @@ class CIAMessage:
         envelope = json.dumps(structure)
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            sock.sendto(envelope + "\n", (target_server, target_port))
+            sock.sendto(envelope.encode() + b"\n", (target_server, target_port))
         finally:
             sock.close()
 

ciabot/run-libreoffice-ciabot.pl
@@ -89,7 +89,7 @@ sub report($$$) {
     my %old = %{$old_ref};
     my %new = %{$new_ref};
     my $ciabot = "timeout 60 $cwd/libreoffice-ciabot.pl";
-    my $ciaproxy = "| ( cd $cwd && python irker-cia-proxy.py -s )";
+    my $ciaproxy = "| ( cd $cwd && python3 irker-cia-proxy.py -s )";
 
     foreach my $key ( keys %new ) {
         my $branch_name = $key;
@@ -127,13 +127,13 @@ sub report($$$) {
                                         my $branch = $branch_name;
                                         $branch = 'master' if ($branch eq '');
                                         print "reporting to bugzilla: $_ and branch $branch\n";
-                                        qx(python $cwd/libreoffice-bugzilla2.py -r $repo -c $_ -b $branch >>/var/log/ciabot/bugzilla.log);
+                                        qx(python3 $cwd/libreoffice-bugzilla2.py -r $repo -c $_ -b $branch >>/var/log/ciabot/bugzilla.log);
                                     }
                                     qx($ciabot $repo $_ $branch_name $ciaproxy);
                                 }
                             } else {
                                 if ( is_valid_bugzilla_commit( $repo, $branch_name ) ) {
-                                    print "python $cwd/libreoffice-bugzilla2.py -r '$repo' -c '$_' -b '$branch_name'\n";
+                                    print "python3 $cwd/libreoffice-bugzilla2.py -r '$repo' -c '$_' -b '$branch_name'\n";
                                 }
                                 print "$ciabot '$repo' '$_' '$branch_name' $ciaproxy\n";
                             }

commit 4101d4e9d8cbb5c0df8ee3232bd1e3af06897573

ciabot: Fix str vs. int comparison.
    Traceback (most recent call last):
      […]
      File "/srv/ciabot/prod/libreoffice-bugzilla2.py", line 142, in find_target_version
        if max(beta_list) >= 2:
    TypeError: '>=' not supported between instances of 'str' and 'int'

Regression from f2204d126c2334ccbe061cd27d519316426448c1.  Solved by
turning to int early for bug numbers and maj/min/etc version numbers.

ciabot/libreoffice-bugzilla2.py
@@ -105,7 +105,7 @@ def find_target_version(repo, branch):
     # form libreoffice-x-y-z => will be available in x.y.z
     match = re.search("libreoffice-(\d+)-(\d+)-(\d+)", branch)
     if match is not None:
-        return ".".join(map(str, match.groups()))
+        return ".".join(match.groups())
 
     # form libreoffice-x-y
     # branch of libreoffice-x-y-z exists => will be available in x.y.z+1
@@ -115,26 +115,26 @@ def find_target_version(repo, branch):
     #       beta
     match = re.search("libreoffice-(\d+)-(\d+)", branch)
     if match is not None:
-        base = ".".join(map(str, match.groups()))
+        base = ".".join(match.groups())
         branches = repo.remote().refs
         branch_names = [str(branch) for branch in branches]
         print(branch_names)
-        search_string = "libreoffice-"+"-".join(map(str, match.groups())) + "-(\d+)"
+        search_string = "libreoffice-" + "-".join(match.groups()) + "-(\d+)"
         print(search_string)
-        micro_list = [m.group(1) for m in [re.search(search_string, branch) for branch in branch_names] if m is not None]
+        micro_list = [int(m.group(1)) for m in [re.search(search_string, branch) for branch in branch_names] if m is not None]
         if len(micro_list) == 0:
             # first search if we are at an RC already
             search_string = "libreoffice-" + base + ".0." + "(\d+)$"
             tags = repo.tags
             print(tags)
-            rc_list = [m.group(1) for m in [re.search(search_string, str(tag)) for tag in tags] if m is not None]
+            rc_list = [int(m.group(1)) for m in [re.search(search_string, str(tag)) for tag in tags] if m is not None]
             print(rc_list)
             if len(rc_list) > 0:
-                return base + ".0." + str(int(max(rc_list)) + 1)
+                return base + ".0." + str(max(rc_list) + 1)
 
             # we have not yet tagged an RC, check which betas have been tagged
             search_string = "libreoffice-" + base + ".0.0.beta(\d+)"
-            beta_list = [m.group(1) for m in [re.search(search_string, str(tag)) for tag in tags] if m is not None]
+            beta_list = [int(m.group(1)) for m in [re.search(search_string, str(tag)) for tag in tags] if m is not None]
             print(beta_list)
             if len(beta_list) == 0:
                 # no beta yet
@@ -145,10 +145,10 @@ def find_target_version(repo, branch):
                 return base + ".0.1"
 
             # normal beta
-            return base + ".0.0.beta" + str(int(max(beta_list)) + 1)
+            return base + ".0.0.beta" + str(max(beta_list) + 1)
         print(micro_list)
         # the next release from libreoffice-x-y is max existing z-branch + 1
-        return base + "." + str(int(max(micro_list)) + 1)
+        return base + "." + str(max(micro_list) + 1)
 
     return None
 
@@ -165,7 +165,7 @@ def find_bugid(repo, commit_id):
         print("no bugid found")
         sys.exit()
 
-    return m
+    return [int(i) for i in m]
 
 def read_repo(repo_name):
     config = configparser.ConfigParser()