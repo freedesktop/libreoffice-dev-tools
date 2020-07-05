aed5bd02b50032e9152f2f91c5632f6b44987c35
Guilhem Moulin <guilhem@libreoffice.org>
2020-07-06 00:17:51 +0200

    ciabot: Move subtree to infra/salt
    The repository now lives at https://git.libreoffice.org/infra/salt/+/master/ciabot/ .
    This makes it easier for the infra team to deploy changes to production.
    Subtree history was preserved during the operation:
    
        https://git.libreoffice.org/infra/salt/history/7162daff402d4ad235ed48e3872bc0b50faa2275

--- ciabot/start-ciabot.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-cd ~/bin
-
-test -n "`ps ax | grep run-libreoffice-ciabot.pl | grep -v grep`" || screen -d -m run-libreoffice-ciabot.pl
--- ciabot/start-irker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ~/bin/irker
-export PYTHONPATH=irc-8.3
-if test -z "`ps ax | grep irkerd | grep -v grep`"; then
-	./irkerd -n loirkerbot &>irkerd.log &
-fi
